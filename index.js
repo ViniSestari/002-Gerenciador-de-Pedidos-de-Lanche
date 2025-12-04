4c39b9b5030949f14f0b8b6da51435be7efb74b7
ViniSestari <vinifujisestari@gmail.com>
2025-12-04 20:35:56 -0300

    Criação de rotas
    criação das rotas
    [get] /listarpedidos
    [get] /listarpedido/:id

--- index.js
+++ index.js
@@ -2,7 +2,48 @@ const express = require('express')
 const app = express()
 
 app.get("/", function(req, res) {
-    res.send("funciona")
+    res.send("está online")
+})
+
+
+let listaDePedidos = [
+    {
+        "id":1,
+        "cliente":"Vinicius",
+        "id_cliente":100,
+        "produto":"X-Bacon",
+        "valor":29.9
+    },
+    {
+        "id":2,
+        "cliente":"Vinicius",
+        "id_cliente":100,
+        "produto":"X-Tudo",
+        "valor":35.5
+    },
+    {
+        "id":3,
+        "cliente":"Neymar",
+        "id_cliente":95,
+        "produto":"X-Verificada",
+        "valor":10.0
+    }
+]
+
+//listar
+app.get("/listarpedidos", function(req, res) {
+    res.send(listaDePedidos)
+})
+
+//listar um
+app.get("/listarpedido/:id",function(req, res){
+    let idinformado = req.params.id
+    if(idinformado > listaDePedidos.length){
+        res.send("Pedido inválido")
+    }else{
+        res.send(listaDePedidos[idinformado - 1])
+    }
 })
 
 app.listen(3000)
+//localhost:3000
